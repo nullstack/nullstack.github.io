e5f544ecc3e4bfdee19029559b5009a75f9c196e
Christian <christian.mortaro@gmail.com>
2021-02-26 22:34:28 -0300

    :rocket:

--- docs/service-worker-aa5ef23cc533f058c164c7201832a37c.js
+++ docs/service-worker-aa5ef23cc533f058c164c7201832a37c.js
@@ -5,7 +5,7 @@ self.context = {
     "development": false,
     "production": true,
     "static": true,
-    "key": "26942e6a3ebf732771349a22420076b3"
+    "key": "aa5ef23cc533f058c164c7201832a37c"
   },
   "project": {
     "type": "website",
